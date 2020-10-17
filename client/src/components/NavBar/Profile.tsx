b230e3ad6e47cb9385aa08d4ecd6574364379436
Rebecca <rebeccaesyria@gmail.com>
2020-10-17 14:32:07 -0400

    remove extra console.log

--- client/src/components/NavBar/Profile.tsx
+++ client/src/components/NavBar/Profile.tsx
@@ -11,7 +11,6 @@ const Profile: React.FunctionComponent = () => {
   const { oidcUser, logout } = useReactOidc();
 
   if (!oidcUser) return null;
-  console.log(oidcUser);
   const {
     profile: { name, preferred_username },
   } = oidcUser;
