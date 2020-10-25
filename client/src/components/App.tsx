7a23bda665700aa546f309351d3eea535c5e49b4
Rebecca <rebeccaesyria@gmail.com>
2020-10-25 00:52:39 -0400

    create page

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { withOidcSecure } from "@axa-fr/react-oidc-context";
-import { Home, Vote } from "./index";
+import { Home, Vote, Create } from "./index";
 import PageContainer from "../containers/PageContainer";
 
 class App extends Component {
@@ -16,6 +16,7 @@ class App extends Component {
               path="/vote/:voteId"
               component={withOidcSecure(Vote)}
             />
+            <Route exact path="/create" component={withOidcSecure(Create)} />
           </Switch>
         </PageContainer>
       </Router>
